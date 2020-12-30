7f88f5d7a7b1799005706d9b1ad99420db0f8b87
Ryan Huellen <ryan@getpassly.com>
2020-12-30 13:02:57 -0600

    UAPI Wrapper Boiler Plate

---
+++ src/modules/cPGreyList/index.ts
@@ -0,0 +1,13 @@
+// Import types and execute method from module.
+import { FunctionEP, mexecute } from '..';
+
+// Import Functions
+
+export const execute = (ep: FunctionEP) => {
+  return mexecute({
+    module: 'cPGreyList',
+    functionEP: ep
+  });
+};
+
+export {};
