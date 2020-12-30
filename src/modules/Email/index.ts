0989305295fc9d57c33b8312ad72f5018d943568
Ryan Huellen <ryan@getpassly.com>
2020-12-30 00:01:53 -0600

    Fixed exports.

--- src/modules/Email/index.ts
+++ src/modules/Email/index.ts
@@ -28,6 +28,76 @@ import deleteheldmessages from './deleteheldmessages';
 import deletelist from './deletelist';
 import deletemx from './deletemx';
 import deletepop from './deletepop';
+import disablefilter from './disablefilter';
+import disablemailboxautocreate from './disablemailboxautocreate';
+import disablespamassasin from './disablespamassasin';
+import disablespamautodelete from './disablespamautodelete';
+import disablespambox from './disablespambox';
+import dispatchclientsettings from './dispatchclientsettings';
+import editpopquota from './editpopquota';
+import enableautospamdelete from './enableautospamdelete';
+import enablefilter from './enablefilter';
+import enablemailboxautocreate from './enablemailboxautocreate';
+import enablespamassassin from './enablespamassassin';
+import enablespambox from './enablespambox';
+import fetchcharmaps from './fetchcharmaps';
+import ftsrescanmailbox from './ftsrescanmailbox';
+import generatemailmanotp from './generatemailmanotp';
+import getautoresponder from './getautoresponder';
+import getcharsets from './getcharsets';
+import getclientsettings from './getclientsettings';
+import getdefaultemailquota from './getdefaultemailquota';
+import getdefaultemailquotamib from './getdefaultemailquotamib';
+import getdiskusage from './getdiskusage';
+import getfilter from './getfilter';
+import getheldmessagecount from './getheldmessagecount';
+import getliststotaldiskusage from './getliststotaldiskusage';
+import getmailboxautocreate from './getmailboxautocreate';
+import getmailmandelegates from './getmailmandelegates';
+import getmainaccountdiskusage from './getmainaccountdiskusage';
+import getmainaccountdiskusagebytes from './getmainaccountdiskusagebytes';
+import getmaxemailquota from './getmaxemailquota';
+import getmaxemailquotamib from './getmaxemailquotamib';
+import getpopquota from './getpopquota';
+import getspamsettings from './getspamsettings';
+import getwebmailsettings from './getwebmailsettings';
+import hasdelegatedmailmanlists from './hasdelegatedmailmanlists';
+import hasplaintextauthentication from './hasplaintextauthentication';
+import holdoutgoing from './holdoutgoing';
+import index from './index';
+import isinteger from './isinteger';
+import listautoresponders from './listautoresponders';
+import listdefaultaddress from './listdefaultaddress';
+import listdomainforwarders from './listdomainforwarders';
+import listfilters from './listfilters';
+import listfiltersbackups from './listfiltersbackups';
+import listforwarders from './listforwarders';
+import listforwardersbackups from './listforwardersbackups';
+import listlists from './listlists';
+import listmaildomains from './listmaildomains';
+import listpops from './listpops';
+import listpopswithdisk from './listpopswithdisk';
+import listsystemfilterinfo from './listsystemfilterinfo';
+import list_mxs from './list_mxs';
+import passwdlist from './passwdlist';
+import passwdpop from './passwdpop';
+import releaseoutgoing from './releaseoutgoing';
+import removemailmandelegates from './removemailmandelegates';
+import reorderfilters from './reorderfilters';
+import setalwaysaccept from './setalwaysaccept';
+import setdefaultaddress from './setdefaultaddress';
+import setlistprivacyoptions from './setlistprivacyoptions';
+import statsdbstatus from './statsdbstatus';
+import storefilter from './storefilter';
+import suspendincoming from './suspendincoming';
+import suspendlogin from './suspendlogin';
+import suspendoutgoing from './suspendoutgoing';
+import tracedelivery from './tracedelivery';
+import tracefilter from './tracefilter';
+import unsuspendincoming from './unsuspendincoming';
+import unsuspendlogin from './unsuspendlogin';
+import unsuspendoutgoing from './unsuspendoutgoing';
+import verifypassword from './verifypassword';
 
 export const execute = (ep: FunctionEP) => {
   return mexecute({
@@ -35,19 +105,6 @@ export const execute = (ep: FunctionEP) => {
     functionEP: ep
   });
 };
-
-// Remove after.
-import * as fs from 'fs';
-let thing = '';
-let exportt = 'export { ';
-fs.readdirSync('./src/modules/Email').forEach((t) => {
-  thing += `import ${t.split('.')[0]} from './${t.split('.')[0]}'\n`;
-  exportt += `${t.split('.')[0]}, `;
-});
-
-console.log(thing);
-console.log(`${exportt.substring(0, exportt.length - 9)} }`);
-// Remove after.
 
 export {
   accountname,
@@ -75,5 +132,75 @@ export {
   deleteheldmessages,
   deletelist,
   deletemx,
-  deletepop
+  deletepop,
+  disablefilter,
+  disablemailboxautocreate,
+  disablespamassasin,
+  disablespamautodelete,
+  disablespambox,
+  dispatchclientsettings,
+  editpopquota,
+  enableautospamdelete,
+  enablefilter,
+  enablemailboxautocreate,
+  enablespamassassin,
+  enablespambox,
+  fetchcharmaps,
+  ftsrescanmailbox,
+  generatemailmanotp,
+  getautoresponder,
+  getcharsets,
+  getclientsettings,
+  getdefaultemailquota,
+  getdefaultemailquotamib,
+  getdiskusage,
+  getfilter,
+  getheldmessagecount,
+  getliststotaldiskusage,
+  getmailboxautocreate,
+  getmailmandelegates,
+  getmainaccountdiskusage,
+  getmainaccountdiskusagebytes,
+  getmaxemailquota,
+  getmaxemailquotamib,
+  getpopquota,
+  getspamsettings,
+  getwebmailsettings,
+  hasdelegatedmailmanlists,
+  hasplaintextauthentication,
+  holdoutgoing,
+  index,
+  isinteger,
+  listautoresponders,
+  listdefaultaddress,
+  listdomainforwarders,
+  listfilters,
+  listfiltersbackups,
+  listforwarders,
+  listforwardersbackups,
+  listlists,
+  listmaildomains,
+  listpops,
+  listpopswithdisk,
+  listsystemfilterinfo,
+  list_mxs,
+  passwdlist,
+  passwdpop,
+  releaseoutgoing,
+  removemailmandelegates,
+  reorderfilters,
+  setalwaysaccept,
+  setdefaultaddress,
+  setlistprivacyoptions,
+  statsdbstatus,
+  storefilter,
+  suspendincoming,
+  suspendlogin,
+  suspendoutgoing,
+  tracedelivery,
+  tracefilter,
+  unsuspendincoming,
+  unsuspendlogin,
+  unsuspendoutgoing,
+  verifypassword
 };
